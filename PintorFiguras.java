d446cd9e9ad6ca30d5bbf874e7c7f1cf1224f26d
Ander <agaonaddam1@gmail.com>
2020-11-09 21:24:43 +0100

    Terminado PintorFiguras

--- PintorFiguras.java
+++ PintorFiguras.java
@@ -20,14 +20,28 @@ public class PintorFiguras
      * (usa bucles for)
      */
     public  void dibujarFigura(int altura) {
-        
+        int espacios = (altura * 2) - 2;
+        for (int i = 1; i <= altura; i++){
+            escribirEspacios(ESPACIO,espacios);
+            for (int j = 1; j <= i; j++){
+                if (j % 2 == 0){
+                    escribirEspacios(CAR2,ANCHO_BLOQUE);
+                }
+                else{
+                    escribirEspacios(CAR1,ANCHO_BLOQUE);
+                }
+            }
+            espacios -= 2;
+            System.out.println();
+        }
     }
 
     /**
      * Método privado de ayuda que escribe n espacios en la misma línea
      */
     private  void escribirEspacios(char caracter, int n) {
-         
-
+        for (int i = 0; i < n; i++){
+            System.out.print(caracter);
+        }
     }
 }
